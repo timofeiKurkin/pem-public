4d6dc1f697c5ad19b1c1b61ea5e24d26ff734ac7
timofey <ydepression83@gmail.com>
2022-11-13 11:21:30 +0700

    deploy web-site v 1.1

--- pages/api/project.js
+++
@@ -1,7 +0,0 @@
-import project from './_projectsInfo/projectsInfo.json'
-
-export default function handler (req, res) {
-	if(req.method === "GET") {
-		res.status(200).json(project)
-	}
-}
--- styles/indexPage/MainSectionStyle.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const MainSectionStyle = styled.main `
-	display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: repeat(1, minmax(500px, auto));
-  //grid-row-gap: 80px;
-`
